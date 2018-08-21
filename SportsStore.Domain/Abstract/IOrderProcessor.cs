6a836b6fa0c742d034dc8b95ac3c28e95a8d9edf
User <User@WIN-GQOKRARBRRP>
2018-08-21 18:03:11 +0300

    Chapter 9: "Implementing the Order Processor" + "Defining the Interface" + "Implementing the Interface" + "Registering the Implementation" + "Completing the Cart Controller" + "Displaying Validation Errors"

---
+++ SportsStore.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,1 @@
+﻿using SportsStore.Domain.Entities;namespace SportsStore.Domain.Abstract{    public interface IOrderProcessor    {        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);    }}
